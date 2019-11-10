a5e3b7c248ca1fe41c85ad47806a3ca050974aeb
edadasko <edadasko@gmail.com>
2019-11-10 22:11:06 +0300

    add photos to posts

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using SimpleSocialNetwork.Models;
+using SimpleSocialNetwork.ViewModels;
 
 namespace SimpleSocialNetwork.Controllers
 {
@@ -14,13 +17,16 @@ namespace SimpleSocialNetwork.Controllers
         const int pageSize = 5;
 
         IUsersRepository _repository;
+        IHostingEnvironment _environment;
         User _user;
 
         public PostController(IUsersRepository repository,
                               IHttpContextAccessor httpContextAccessor,
+                              IHostingEnvironment environment,
                               UserManager<User> userManager)
         {
             _repository = repository;
+            _environment = environment;
             var id = userManager.GetUserId(httpContextAccessor.HttpContext.User);
             _user = _repository.GetUserById(id);
         }
@@ -31,20 +37,40 @@ namespace SimpleSocialNetwork.Controllers
             return PartialView(post);
         }
 
-        [Route("createPost/{text}")]
-        public ActionResult Create(string text)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(PostViewModel postVM)
         {
+            if (postVM.Text == null)
+                return RedirectToAction("PostsList");
+
+
             var post = new Post()
             {
                 Owner = _user,
                 Date = DateTime.Now,
-                Text = text,
+                Text = postVM.Text,
                 Type = PostType.Normal
             };
+
+            if (postVM.Photo != null)
+                await AddPhotoAsync(postVM.Photo, post);
 
             _repository.Create(post);
             _repository.Save();
             return RedirectToAction("PostsList");
+        }
+
+        private async Task AddPhotoAsync(IFormFile photo, Post post)
+        {
+            string path = "/usersPostPhotos/post" + _user.Id + DateTime.Now.ToString("MMddyyyyHHmmss") + ".jpg";
+            using (var fileStream = new FileStream(_environment.WebRootPath + path, FileMode.Create))
+            {
+                await photo.CopyToAsync(fileStream);
+            }
+
+            Photo photoToSave = new Photo { Image = path, Post = post };
+            _repository.Create(photoToSave);
         }
 
         [Route("removePost/{postId}")]
